792395563cec33263f31da37697658db17a0c641
Vinicius Schilieve Santos <47587017844@info.local>
2024-08-21 19:28:09 -0300

    Novas atualizações

--- Sistema.py
+++ Sistema.py
@@ -11,9 +11,10 @@ from datetime import date,datetime
 
 def main():
     lista_pf=[]
+    lista_pj =[]
 
     while True:
-        opcao = int(input("Escolha uma opcao: 1 - Pessoa Fisica/ 2 - Pessoa Juridica/0 - Sair"))
+        opcao = int(input("Escolha uma opcao: 1 - Pessoa Fisica/ 2 - Pessoa Juridica/ /3-Atualizar lista/0 - Sair"))
 
         if opcao ==1:
             while True:
@@ -80,27 +81,62 @@ def main():
                                break
                             if not pessoa_encontrada:
                                 print("Nenhuma pessoa encontrada")
-                    
+                
+                
+              # Atualização de dadados do cadastro
+
+
+                      # Atualizar Lista
+                if opcao_pf == 4:
+                    cpf_para_atualizar = input("Digite o CPF que deseja atualizar: ")
+                    pessoa_encontrada = False
+
+                    for cada_pf in lista_pf:
+                        if cada_pf.cpf == cpf_para_atualizar:
+                            pessoa_encontrada = True
+
+                            print("Escolha qual dado deseja atualizar")
+                            print("N - Nome")
+                            print("R - Rendimento")
+                            print("L - Logradouro")
+                            print("M - Numero")
+
+                            escolha = input("Digite a inicial do atributo que deseja alterar:")
+
+                            if escolha == "N":
+                                novo_nome = input(f"O nome atual é {cada_pf.nome}. Digite o novo nome para atualizar: ")
+                                cada_pf.nome = novo_nome
+                            elif escolha == "R":
+                                novo_rendimento = input(f"O rendimento atual é {cada_pf.rendimento}. Digite o novo valor do rendimento: ")
+                                cada_pf.rendimento = novo_rendimento
+                            elif escolha == "L":
+                                novo_logradouro = input(f"O logradouro atual é {cada_pf.logradouro}. Digite o novo logradouro: ")
+                                cada_pf.logradouro = novo_logradouro
+                            elif escolha == "M":
+                                novo_numero = input(f"O número atual é {cada_pf.numero}. Digite o novo número para atualizar")
+                                cada_pf.numero = novo_numero
+                            else:
+                                print("Opção inválida!")
+                                break
+                        
+                            
 
                     
                         
 
                 
                 
-                
+            
                 
                 elif opcao_pf ==0:
-
-
-
                     print("Voltando ao menu Anteriro")
                     break 
                 else:
                     print("Opcao invalida, por favor digite uma das opcoes indicadas")
                     # PESSOA JURIDICA 
-                    
+
         elif opcao ==2:
-            lista_pj =[]
+            
             while True:
                 opcao_pj = int(input("Escolha uma opcao: 1 - Cadastrar Pessoa Juridica/ 2 - Listar Pessoa Juridica/ 3- exluir CNPJ da lista/ 0 - Voltar ao menu anterior"))
                  #cadastrar uma pessoa Juridica
@@ -145,7 +181,7 @@ def main():
                 
                 elif opcao_pj == 3:
 
-                    cnpj_para_remover = input("Digite o cnpj")
+                    cnpj_para_remover = input("Digite o cnpj que deseja remover")
                     empresa_encontrada = False
                                 
 
@@ -155,19 +191,46 @@ def main():
                                empresa_encontrada = True
                                print("Pessoa Juridica Removida")
                                break
-                            if not empresa_encontrada:
-                                print("Nenhuma pessoa Juridica encontrada")
+                    if not empresa_encontrada:
+                        print("Nenhuma pessoa Juridica encontrada")
                     
-
-            print("Funcionalidades para pessoa juridica nao implementada")
         elif opcao == 0:
             print("Obrigado por utilizar o nosso sistema!!!Valeu")
+            # Atualizar Lista
+            if opcao_pj == 4:
+                    if lista_pj:
+                        cnpj_para_atualizar = input("Digite o CNPJ da Empresa que deseja atualizar:")
+                        for cada_pj in lista_pj:
+                            if cada_pj.cnpj == cnpj_para_atualizar:
+                                novo_rendimento = float(input("Digite o novo rendimento: "))
+                                cada_pj.rendimento = novo_rendimento
+                                print("Rendimento atualizado com sucesso!")
+                                break
+                        else:
+                            print("Nenhum CNPJ encontrado")
+
+
+                # Sair do menu Atual
+            elif opcao_pf == 0:
+                    print("Voltando ao menu anterior")
+                    break 
+
+            else: 
+                    print("Opção inválida, por favor digite uma das opções abaixo indicadas.")      
+
+            print("Funcionalidades para pessoa jurídica não implementadas.")
+            pass
+
+
+        elif opcao == 0:
+            print("Obrigado por utilizar nosso sistema!")
+            break
             break 
         
         else:
             print("Opcao invalida, por favor digite uma das opcoes validas")
                     
-#Pessoa Juridica
+
     
        
 
